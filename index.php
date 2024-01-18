aff8eee24a868ac509e43168f78c1f2d77261d00
chanemon <chandradura05@gmail.com>
2024-01-18 18:56:49 +0545

    added slider using swiperjs.com->autoplay-.core bata link,script(loop ma)use garera

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CHANDRAMUKHI HOTEL</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="assets/css/style.css">
+   
 </head>
+
 <body>
 <!--header section start-->
 <header>
@@ -41,42 +44,48 @@
 
 <!--home secction starts-->
 <section class="home" id="home">
-    <div class=" home-slider">
-        <div class="wrapper">
-           <div class="slide">
+
+    <div class=" swiper ">
+
+        <div class="swiper-wrapper wrapper">
+
+           <div class="swiper-slide slide">
+
             <div class="content">
                 <span>our special dishes</span>
                 <h3>pizza</h3>
                 <p>For lunch,we have pizza.Peperoni pizza is very delicious.</p>
-                <a href="#" class="btn"></a>
+                <a href="#" class="btn">order now</a>
             </div>
             <div class="image">
                 <img src="assets/uploads/pizza.jpg" alt="pizza" width="500px" height="500px">
             </div>
            </div> 
-           <div class="slide">
+           <div class="swiper-slide slide">
             <div class="content">
                 <span>our special dishes</span>
                 <h3>cake</h3>
                 <p>For desert, we have vanilla cake</p>
-                <a href="#" class="btn"></a>
+                <a href="#" class="btn">order now</a>
             </div>
             <div class="image">
                 <img src="assets/uploads/cake.jpg" alt="cake">
             </div>
            </div> 
-           <div class="slide">
+           <!--sliderhaleko-->
+           <div class="swiper-slide slide">
             <div class="content">
                 <span>our special dishes</span>
                 <h3>Whole chicken</h3>
                 <p>For dinner, we have whole chicken.</p>
-                <a href="#" class="btn"></a>
+                <a href="#" class="btn">order now</a>
             </div>
             <div class="image">
                 <img src="assets/uploads/wholechicken.jpg" alt="wholechicken" >
             </div>
-           </div> 
+           </div>       
         </div>
+        <div class="swiper-pagination"></div>   
     </div>
 </section>
    
@@ -89,7 +98,7 @@
 
 
 
-
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <!--custom script.js link-->
 <script src="assets/js/main.js"></script>
 </body>
